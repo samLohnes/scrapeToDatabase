a107d3735f48961cfabb39ddbcb451564491b9a8
sam <sam_lohnes@icloud.com>
2023-08-15 09:04:44 -0700

    created functions for the csvToPSQL.py file

--- csvToPSQL.py
+++ csvToPSQL.py
@@ -1,1 +1,10 @@
 # This is where the csv file will be read into a PSQL db utilizing Pandas and SQLAlchemy
+
+import sqlalchemy
+import pandas as pd
+
+def setClass():
+    return
+
+def uploadCSV():
+    return
